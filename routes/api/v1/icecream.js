e07891c86b51910d6b55eb5e83e2191c598f5913
Faheer Hakim <r0820158@student.thomasmore.be>
2026-06-07 18:15:41 +0200

    feat: voeg v1 icecream routes toe met tijdelijke controller handlers

--- routes/api/v1/icecream.js
+++ routes/api/v1/icecream.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const icecreamController = require('../../../controllers/api/v1/icecream');
+
+
+router.get("/", todosController.getAll);
+router.get("/", todosController.getOne);
+router.post("/", todosController.create);
+router.put("/:id", todosController.update);
+router.delete("/:id", todosController.remove);
+
+
+module.exports = router;
