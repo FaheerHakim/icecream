67707094120985d497d924c4b0f5de6b1ec1a328
Faheer Hakim <r0820158@student.thomasmore.be>
2026-06-09 00:59:01 +0200

    fix: voeg cors middleware toe aan app.js voor cross-origin requests

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -13,6 +14,8 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://127.0.0.1:27017/icecream');
 const app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
