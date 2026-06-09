be015a7620971447664475054cce02d945b5e9fe
Faheer Hakim <r0820158@student.thomasmore.be>
2026-06-09 21:20:04 +0200

    :id voor getOne route is toegevoegd

--- routes/api/v1/icecream.js
+++ routes/api/v1/icecream.js
@@ -4,7 +4,7 @@ const icecreamController = require('../../../controllers/api/v1/icecream');
 
 
 router.get("/", icecreamController.getAll);
-router.get("/", icecreamController.getOne);
+router.get("/:id", icecreamController.getOne);
 router.post("/", icecreamController.create);
 router.put("/:id", icecreamController.update);
 router.delete("/:id", icecreamController.remove);
